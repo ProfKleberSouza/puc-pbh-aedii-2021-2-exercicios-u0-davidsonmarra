afe92978b33c1cbf356aad9f696e2cfe2379c6cc
Davidson <davidsonmarra@gmail.com>
2021-08-06 18:34:27 -0300

    add exe02

--- exe02/main.c
+++ exe02/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   // ENTRADAS: tempo, velocidade media e rendimento
+   int t, v, r;
+
+   // PROCESSAMENTO
+   scanf("%i %i %i", &t, &v, &r);
+   int d = v * t;
+   int l = d / r;
+
+   // SAIDAS
+   printf("R = %i\nV = %i\nT = %i\nD = %i\nL = %i", r, v, t, d, l);
    return 0;
 }
