1e6396b5d6499044cd6b4335b1e7ee659d7bfbfd
Davidson <davidsonmarra@gmail.com>
2021-08-05 23:25:59 -0300

    add exe01

--- exe01/main.c
+++ exe01/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main() {
+   // ENTRADAS: 2 numeros inteiros
    int n1, n2;
+
+   // PROCESSAMENTO: a soma entre esses 2 últimos numeros
    scanf("%i %i", &n1, &n2);
+
+   // SAIDA: o resultado
    printf("SOMA = %i\n", n1+n2);
    return 0;
 }
