e848706bb3a4db55545a96c8892dd4dc3fd551d0
Davidson <davidsonmarra@gmail.com>
2021-08-07 21:49:49 -0300

    add exe05

--- exe05/main.c
+++ exe05/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   // ENTRADAS: 4 notas no formato de numero inteiro
+   int n1, n2, n3, n4;
+   scanf("%i %i %i %i", &n1, &n2, &n3, &n4);
+
+   // PROCESSAMENTO: media das notas com 1 casa decimal
+   float media;
+   printf("NOTA = "); // SAIDA
+   media = (n1 + n2 + n3 + n4) / 4.0; // calcula a media
+
+   // SAIDA
+   media >= 6 
+   ? printf("%.1f (APROVADO)", media) // se a media for maior ou igual a 6
+   : printf("%.1f (REPROVADO)", media); // se a media for menor que 6
    return 0;
 }
