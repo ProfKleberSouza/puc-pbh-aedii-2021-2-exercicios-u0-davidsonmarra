1bed4fb207e90059171d9552fbbec80fd5b57dd1
Davidson <davidsonmarra@gmail.com>
2021-08-07 22:34:58 -0300

    add exe06

--- exe06/main.c
+++ exe06/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   // ENTRADAS: os 3 lados de um triangulo
+   int x, y, z;
+   scanf("%i %i %i", &x, &y, &z);
+
+   // PROCESSAMENTO
+   if(x + y < z || x + z < y || y + z < x || x <= 0 || y <= 0 || z <= 0) 
+      printf("OS LADOS NAO FORMAM UM TRIANGULO"); // SAIDA
+   else if(x == y && x == z) 
+      printf("TRIANGULO EQUILATERO"); // SAIDA
+   else if(x == y || x == z || y == z)
+      printf("TRIANGULO ISOSCELES"); // SAIDA
+   else
+      printf("TRIANGULO ESCALENO"); // SAIDA 
    return 0;
 }
