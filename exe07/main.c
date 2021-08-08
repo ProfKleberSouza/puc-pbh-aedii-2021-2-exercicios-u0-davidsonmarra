5fa73e14a4d1eaf82cfbdd83496d0475959e4257
Davidson <davidsonmarra@gmail.com>
2021-08-07 23:39:56 -0300

    add exe07

--- exe07/main.c
+++ exe07/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   // ENTRADAS: um numero inteiro N 
+   int N;
+   scanf("%i", &N);
+   int aux; 
+   scanf("%i", &aux); 
+   int maior = aux, menor = aux;
+   for(int i = 0; i < N - 1; i++) {
+      scanf("%i", &aux);
+      aux > maior ? maior = aux : ""; // PROCESSAMENTO
+      aux < menor ? menor = aux : ""; // PROCESSAMENTO
+   }
+
+   // SAIDA
+   printf("MENOR = %i\nMAIOR = %i", menor, maior);
    return 0;
 }
