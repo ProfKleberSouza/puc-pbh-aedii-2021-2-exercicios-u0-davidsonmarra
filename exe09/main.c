b7f2f16aae06814e2c61363bdeb6c4fc3ccae8df
Davidson <davidsonmarra@gmail.com>
2021-08-12 20:42:39 -0300

    add memoria estatica de forma segura

--- exe09/main.c
+++ exe09/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define N 100
 
 struct Alunos {
    char nome[50];
@@ -10,9 +11,9 @@ struct Alunos {
 /* Descricao: procedimento que preenche o vetor de alunos e calcula a media das notas
 Entradas: o vetor de struct alunos, o numero inteiro de posicoes do vetor e o endereco da variavel que armazenara a media das notas 
 Saidas: o vetor preenchido e a media retornada por referencia */
-void preencheVetor(struct Alunos alunos[], int N, float * media) {
+void preencheVetor(struct Alunos alunos[], int n, float * media) {
    float aux = 0;
-   for(int i = 0; i < N; i++) {
+   for(int i = 0; i < n; i++) {
       scanf("%s %f", &alunos[i].nome, &alunos[i].nota);
       aux += alunos[i].nota;
    }
@@ -22,13 +23,13 @@ void preencheVetor(struct Alunos alunos[], int N, float * media) {
 /* Descricao: procedimento que preenche o vetor maiores com os 3 alunos que tiveram a maior nota
 Entradas: vetor de struct alunos já preenchido, vetor de struct de maiores notas vazio, e a quantidade de alunos
 Saida: vetor de maiores preenchido com os 3 alunos que tiveram as maiores notas */
-void melhoresAlunos(struct Alunos alunos[], struct Alunos maiores[], int N) {
+void melhoresAlunos(struct Alunos alunos[], struct Alunos maiores[], int n) {
    int i, j, menorIndex = 0;
    for(i = 0; i < 3; i++) { // inicializa o vetor com as primeiras notas do vetor de alunos
       maiores[i].nota = alunos[i].nota;
       strcpy(maiores[i].nome, alunos[i].nome);
    }
-   for(i = 3; i < N; i++) {
+   for(i = 3; i < n; i++) {
       for(j = 0; j < 3; j++) 
          if(maiores[menorIndex].nota > maiores[j].nota)
             menorIndex = j;
@@ -82,14 +83,14 @@ void ordemAlfabetica(struct Alunos maiores[]) {
 
 int main() {
    // ENTRADAS: o numero de alunos, o nome dos alunos e a nota deles
-   int N;
+   int n;
    float media;
-   scanf("%i", &N);
+   scanf("%i", &n);
    struct Alunos vetAlunos[N], maiores[3]; // cria um vetor de alunos de N posicoes e um vetor para guardar os 3 melhores alunos
 
    // PROCESSAMENTO
-   preencheVetor(vetAlunos, N, &media); // o vetor eh preenchido
-   melhoresAlunos(vetAlunos, maiores, N); // filtra os 3 maiores notas
+   preencheVetor(vetAlunos, n, &media); // o vetor eh preenchido
+   melhoresAlunos(vetAlunos, maiores, n); // filtra os 3 maiores notas
    printf("NOTA MEDIA = %.1f\n", media); // SAIDA
    ordemAlfabetica(maiores); // printa na tela em ordem alfabetica
    return 0;
